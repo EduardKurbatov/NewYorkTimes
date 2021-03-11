28ed604c649ab563c7c854ec9b15d971a0b609d3
Edik123123 <edikcrimedog@gmail.com>
2021-03-11 17:16:06 +0200

    geting data with API

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,39 @@
 import './Main.scss';
 
+function execute() {
+  const url = "https://api.nytimes.com/svc/mostpopular/v2/shared/1/facebook.json?api-key=oLLGAGDyC2xECFJqIDKqxlczH0fE3gGO";
+  const options = {
+    method: "GET",
+    headers: {
+      "Accept": "application/json"
+    },
+  };
+  fetch(url, options).then(
+    response => {
+      if (response.ok) {
+        return response.json();
+      }
+      return response.text().then(err => {
+        return Promise.reject({
+          status: response.status,
+          statusText: response.statusText,
+          errorMessage: err,
+        });
+      });
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
 function Main() {
     return (
         <div className="main-page">
             <h1>This is main page</h1>
+            <button onClick={execute}>Exe</button>
         </div>
     )
 }
