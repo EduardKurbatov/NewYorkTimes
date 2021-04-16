344713247a3e52e8d24b2ecfc8ad385754c9ae22
Edik123123 <edikcrimedog@gmail.com>
2021-04-16 17:58:28 +0300

    added styles for header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import './Header.scss';
 import { useHistory } from 'react-router-dom';
 import defaultPhoto from '../../assets/nouserimg.jpg';  
 import fire from '../../fire';
+import 'ionicons-npm//css/ionicons.css';
 
 type Props = {
   user: any,
@@ -23,16 +24,17 @@ const Header = ({setUser, user, hasAccount}: Props) => {
       <h1 className="header" onClick={() => {history.push('/')}}>NY Times</h1>
       {user ? (
         <div className="exist-user">
-          <img className="user-avatar" src={user.photoURL || defaultPhoto} alt="User avatar" />
-          <div className="drop-down">
-            <div className="drop-down-item">
-              <button className="drop-down-btn" onClick={() => {history.push('/profile')}}>Profile</button>
-              <button className="drop-down-btn" onClick={handleLogOut}>LogOut</button>
-            </div>
-          </div>  
+          <div className="avatar-wrapper">
+            <img className="user-avatar" src={user.photoURL || defaultPhoto} alt="User avatar" />
+            <i className="ion-chevron-down"></i>
+          </div>
+          <div className="drop-down-items">
+            <button className="drop-down-item" onClick={() => {history.push('/profile')}}>Profile<i className="ion-android-person"></i></button>
+            <button className="drop-down-item" onClick={handleLogOut}>Logout<i className="ion-log-out"></i></button>
+          </div>
         </div>
       ) : (
-        <button className="login-btn" onClick={() => {history.push('/sign')}}>Login</button>
+        <button className="login-btn" onClick={() => {history.push('/sign')}}><i className="ion-ios-personadd"></i></button>
       )}
     </div>
   );
